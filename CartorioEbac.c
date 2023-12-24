1a4edfe4f75ba68f447c55ffe5aad79ac39b1bec
ArthurCFred <154742064+ArthurCFred@users.noreply.github.com>
2023-12-24 14:18:23 -0300

    Opcao de saida
    Foi adicionada uma 4a opcao para sair do sistema

--- CartorioEbac.c
+++ CartorioEbac.c
@@ -125,7 +125,8 @@ int main()
 		printf("Escolha a opção desejada do menu:\n\n");
 		printf("\t1 - Registrar nomes\n");
 		printf("\t2 - Consultar nomes\n");
-		printf("\t3 - Deletar nomes\n\n");
+		printf("\t3 - Deletar nomes\n");
+		printf("\t4 - Sair do Programa\n\n");
 		printf("Digite o numero da opção desejada:\n\n"); //Fim do menu
 	
 		scanf("%d", &opcao); //armazenando escolha do usuario
@@ -145,6 +146,11 @@ int main()
 			
 			case 3:
 			deletar();
+			break;
+			
+			case 4:
+			printf("\n\n################################################\nObrigado por utilizar o programa, volte sempre!\n################################################\n\n\n");
+			return 0;
 			break;
 			
 			default:
